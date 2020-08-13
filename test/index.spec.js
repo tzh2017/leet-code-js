13ad21a1d1afa9b6741f9870cd610203c01ffb1f
田志会-Chris <chris.tian@biposervice.com>
2020-08-13 19:31:50 +0800

    init

---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+var chai = require("chai");
+var expect = chai.expect;
+
+var main = require("../lib/0136-single-number");
+
+describe(main.name, function () {
+  it("calc", function () {
+    expect(main.func).equal(1);
+  });
+});
