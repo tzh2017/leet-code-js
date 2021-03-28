1a09595a632289930fc06d642b5a62676ebdca57
田志会 Chris <chris.tian@biposervice.com>
2021-03-28 22:43:45 +0800

    feat: use map

--- test/main.spec.js
+++ test/main.spec.js
@@ -5,11 +5,12 @@ var expect = chai.expect;
 
 fs.readdirSync("lib") //
   .map((e) => require(`../lib/${e}`))
+  .filter((e) => !e.ignore)
   .forEach(({ name, fn, params }) => {
     describe(name, function () {
       params().forEach(({ args, target }, i) => {
         it(`test case ${i + 1}`, function () {
-          let result = fn(...args);
+          const result = fn(...args);
           expect(result).to.deep.equal(target);
         });
       });
