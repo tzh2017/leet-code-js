bc5368f9e90845bb7cab323260fdd2c31f9ddd73
田志会 Chris <chris.tian@biposervice.com>
2022-02-21 17:25:38 +0800

    chore

--- test/main.spec.js
+++ test/main.spec.js
@@ -6,9 +6,9 @@ var expect = chai.expect;
 fs.readdirSync("lib") //
   .map((e) => require(`../lib/${e}`))
   .filter((e) => !e.ignore)
-  .forEach(({ name, fn, params }) => {
+  .forEach(({ name, fn, usecases }) => {
     describe(name, function () {
-      params().forEach(({ args, target }, i) => {
+      usecases().forEach(({ args, target }, i) => {
         it(`test case ${i + 1}`, function () {
           const result = fn(...args);
           expect(result).to.deep.equal(target);
